b9db3536e008f37c07eb1f4a1dcdc3360677e651
alaminchowdhury1122 <alonealamin109@gmail.com>
2025-09-27 05:00:18 +0600

    5.00am

--- list.py
+++ list.py
@@ -1,2 +1,22 @@
 friend = ["apple", "orange" , 5, 345, 0.6, "jhon"]
 print(friend)
+print(friend[0])
+
+
+friend[0] = "grapes"
+
+
+print(friend[0])
+
+
+print(friend[0:4])
+
+
+friend.append("jony_deep")
+print(friend)
+
+
+
+l1 = [1,4, 5,6 ,7 ,67, 9, 2,4324,54,55345,543,43,453]
+l1.sort()
+print(l1)
